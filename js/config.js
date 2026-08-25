45ad4971215a02b1be869468419d3b2dd8f7e3ff
DeisonBM <157446105+DeisonBM@users.noreply.github.com>
2026-08-24 19:48:29 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // URL del Web App de Google Apps Script (Implementar > Nueva implementación > Aplicación web)
 const CONFIG = {
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwDvMVoO8vaqXcFn7Km2kB3NwJpKueoj5w-kfsIxEkcAAYBRmtzALdhWNtJOHV7237Qkg/exec',
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyfx13ET4dVNDwhy1rRFCAgurIVsQN3i5CnXjqPJJTFFLGGSSpqzyGd1NlDjC7t8iao/exec',
   // Debe coincidir EXACTO con ADMIN_KEY en Code.gs. Solo se usa en admin.html.
   ADMIN_KEY: 'maria123'
 };
