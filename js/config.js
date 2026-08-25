4a6bc1f47bf8cf9702d9d6e800982924ed90d3d3
DeisonBM <157446105+DeisonBM@users.noreply.github.com>
2026-08-24 22:31:22 -0500

    Change SCRIPT_URL in config.js
    Updated SCRIPT_URL to new Google Apps Script deployment.

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // URL del Web App de Google Apps Script (Implementar > Nueva implementación > Aplicación web)
 const CONFIG = {
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyfx13ET4dVNDwhy1rRFCAgurIVsQN3i5CnXjqPJJTFFLGGSSpqzyGd1NlDjC7t8iao/exec',
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzQPWm2909mCVUsxmT9glVFUAo6m8x00kADt0L_L_9nK2aV-6R103E1fHIZtpIUaWK6Cg/exec',
   // Debe coincidir EXACTO con ADMIN_KEY en Code.gs. Solo se usa en admin.html.
   ADMIN_KEY: 'maria123'
 };
